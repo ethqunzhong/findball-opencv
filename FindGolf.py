a331361ba64c43409a282248e08c5eaea830912c
thiszyq <zyqbit@qq.com>
2016-01-07 20:54:49 +0800

    遍历轮廓&霍夫圆变换

--- FindGolf.py
+++ FindGolf.py
@@ -22,46 +22,17 @@ HSV_MIN = np.array((15, 100, 100))
 HSV_MAX = np.array((45, 255, 255))
 
 
-# In study room
-
-def noting(x):
-    pass
-
-
-#
-# HSV_MIN = np.array((Hlow, Slow, Vlow))
-# HSV_MAX = np.array((Hhigh, Shigh, Vhigh))
-
-# 写一个滑动块动态调节阈值变化参数
-# HSV_MIN = np.array((18, 85, 85))
-# HSV_MAX = np.array((43, 255, 255))
-
-# outdoor
-# HSV_MIN = np.array((20, 90, 90))
-# HSV_MAX = np.array((43, 255, 255))
-
-
+def distance(x1, y1, x2, y2):
+    m = abs(x2 - x1)
+    n = abs(y2 - y1)
+    r = math.sqrt(m * m + n * n)
+    return r
 
 
 def find_ball(capture, noimage, nothreshold):
     global HSV_MIN
     global HSV_MAX
 
-    # cv2.createTrackbar('Hl', 'thresholded image', 15, 255, noting)
-    # cv2.createTrackbar('Sl', 'thresholded image', 90, 255, noting)
-    # cv2.createTrackbar('Vl', 'thresholded image', 90, 255, noting)
-    # cv2.createTrackbar('Hh', 'thresholded image', 45, 255, noting)
-    # cv2.createTrackbar('Sh', 'thresholded image', 200, 255, noting)
-    # cv2.createTrackbar('Vh', 'thresholded image', 200, 255, noting)
-    # Hlow = cv2.getTrackbarPos('Hl', 'thresholded image')
-    # Slow = cv2.getTrackbarPos('Sl', 'thresholded image')
-    # Vlow = cv2.getTrackbarPos('Vl', 'thresholded image')
-    # Hhigh = cv2.getTrackbarPos('Hh', 'thresholded image')
-    # Shigh = cv2.getTrackbarPos('Sh', 'thresholded image')
-    # Vhigh = cv2.getTrackbarPos('Vh', 'thresholded image')
-    #
-    # HSV_MIN = np.array((int(Hlow), int(Slow), int(Vlow)))
-    # HSV_MAX = np.array((int(Hhigh), int(Shigh), int(Vhigh)))
     Cx, Cy = 0, 0
     maxdiag = 0  # 判断面积
     fps = 30  #
@@ -70,8 +41,8 @@ def find_ball(capture, noimage, nothreshold):
     ret, frame = capture.read()  # 获取每一帧
     text = "searching golf..."
 
-    # frame = cv2.resize(frame, (0, 0), fx=0.8, fy=0.8)  # set window's size
-    frame= cv2.resize()
+    frame = cv2.resize(frame, (0, 0), fx=0.8, fy=0.8)  # set window's size
+
     if frame is not None:
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # convert BGR to HSV
         # 色度，饱和度，明度（纯度）
@@ -90,20 +61,6 @@ def find_ball(capture, noimage, nothreshold):
                                                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         pass  # version2.0加入圆检测
-        circles = cv2.HoughCircles(v,cv2.HOUGH_GRADIENT, 1, 20, param1=90,
-                                   param2=40, minRadius=0, maxRadius=100)
-        # circles=np.uint16(np.around(circles))
-        # for i in circles[0,:]:
-        if circles is not None:
-            # convert the (x, y) coordinates and radius of the circles to integers
-            circles = np.round(circles[0, :]).astype("int")
-            # print(circles)
-            # loop over the (x, y) coordinates and radius of the circles
-            for (x, y, r) in circles:
-                # draw the circle in the output image, then draw a rectangle
-                # corresponding to the center of the circle
-                cv2.circle(frame, (x, y), r, (0, 255, 255), 4)
-                # cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
         for cnt in contours:
 
@@ -115,11 +72,28 @@ def find_ball(capture, noimage, nothreshold):
             current_diag = math.sqrt(w * w + h * h) / 2
             current_diag = current_diag / (math.sqrt(2))
             if (current_diag > maxdiag):
-                maxdiag, Cx, Cy = 1.384 * current_diag, int(1.023 * cx), int(1.06 * cy)  # change
+                maxdiag, Cx, Cy = 1.00 * current_diag, int(1.023 * cx), int(1.06 * cy)  # change
 
-        if maxdiag > 0:
-            cv2.circle(frame, (Cx, Cy), int(maxdiag), (0, 255, 0), 3)
-            #
+        circles = cv2.HoughCircles(v, cv2.HOUGH_GRADIENT, 1, 20, param1=90,
+                                   param2=40, minRadius=0, maxRadius=100)
+
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+
+            for (x, y, r) in circles:
+                if distance(x, y, Cx, Cy) < r:
+                    cv2.circle(frame, (x, y), r, (0, 255, 255), 4)
+                    cv2.circle(frame, (x, y), 2, (0, 0, 255), 5)
+                    cv2.circle(frame, (Cx, Cy), int(maxdiag), (0, 255, 0), 3)
+                    cv2.circle(frame, (Cx, Cy), 2, (255, 0, 0), 5)
+
+            # for (x, y, r) in circles:
+            #         cv2.circle(frame, (x, y), r, (0, 255, 255), 4)
+            #         cv2.circle(frame, (x, y), 2, (0, 0, 255), 5)
+            # if maxdiag > 0:
+            #     cv2.circle(frame, (Cx, Cy), int(maxdiag), (0, 255, 0), 3)
+            #     cv2.circle(frame, (Cx, Cy), 2, (255, 0, 0), 5)
+            #     #
             #     (x, y), radius = cv2.minEnclosingTriangle(cnt)
             #     current_diag=int (radius)
             #     if(current_diag>maxdiag):
@@ -136,9 +110,9 @@ def find_ball(capture, noimage, nothreshold):
                     (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         if noimage == False:
-            cv2.imshow("h-image", h)
-            cv2.imshow("s-image", s)
-            cv2.imshow("v-image", v)
+            cv2.imshow("H_channel", h)
+            cv2.imshow("S_channel", s)
+            cv2.imshow("V_channel", v)
             cv2.imshow("original image", frame)
 
         if nothreshold == False:
@@ -164,8 +138,8 @@ def main():
     noimage = False
     nothreshold = False
 
-    print('Initialize Camera...')  # ,
-    capture = cv2.VideoCapture(-1)
+    print('Initialize Camera...')  #
+    capture = cv2.VideoCapture(0)
 
     if capture is not None:
         print('OK...')
